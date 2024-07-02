b84bcf8049adf1d3e8177fb5c806288f16ab891d
MZaFaRM <muhammedzafar.mm@gmail.com>
2024-07-03 03:50:23 +0530

    [FEAT] Completed first iteration

--- wordle.py
+++ wordle.py
@@ -1,8 +1,11 @@
+import contextlib
 import random
 import colorama
 from colorama import Fore
 from string import ascii_lowercase
 from functools import lru_cache
+
+colorama.init(autoreset=True)
 
 
 class InvalidWord(Exception):
@@ -25,8 +28,6 @@ class Wordle:
         self.guessed_wrong = set()
         self.guessed_wrong_index = set()
 
-        colorama.init(autoreset=True)
-
         self.words = ALL_WORDS
         if self.verbose:
             print(Fore.YELLOW + "Dictionary loaded")
@@ -41,8 +42,12 @@ class Wordle:
         for i in range(5):
             if string[i] == self.word[i]:
                 self.guessed_correct.add(string[i])
+                with contextlib.suppress(KeyError):
+                    self.guessed_wrong_index.remove(string[i])
+
             elif string[i] in self.word:
-                self.guessed_wrong_index.add(string[i])
+                if string[i] not in self.guessed_correct:
+                    self.guessed_wrong_index.add(string[i])
             else:
                 self.guessed_wrong.add(string[i])
 
@@ -61,11 +66,13 @@ class Wordle:
                     else:
                         output.append(Fore.BLACK + string[i])
                 output.append(Fore.RESET)
+                output.append("\n")
 
                 output.append(
                     Fore.YELLOW + f"{self.chance_remaining} guess(es) remaining"
                 )
 
+            output.append("\n")
             for i in ascii_lowercase:
                 if i in self.guessed_correct:
                     output.append(Fore.GREEN + i)
@@ -98,14 +105,48 @@ class Wordle:
             self.terminal = True
             self.win = False
 
-            # print(Fore.RED + "You've lost :/ the word was " + Fore.CYAN + self.word)
+            print(Fore.RED + "You've lost :/ the word was " + Fore.CYAN + self.word)
             return True
         elif string == self.word and self.terminal != True:
             self.terminal = True
             self.win = True
-            # print(Fore.GREEN + "Congrats you won!")
+            print(Fore.GREEN + "Congrats you won!")
             return True
         return False
+
+
+class State:
+    def __init__(
+        self, correct_guesses, wrong_index_guesses, wrong_guesses, chances_left
+    ):
+        self.correct_guesses = tuple(correct_guesses)
+        self.wrong_index_guesses = tuple(wrong_index_guesses)
+        self.wrong_guesses = tuple(wrong_guesses)
+        self.chances_left = chances_left
+
+    def __eq__(self, other: object) -> bool:
+        return (
+            self.correct_guesses == other.correct_guesses
+            and self.wrong_index_guesses == other.wrong_index_guesses
+            and self.wrong_guesses == other.wrong_guesses
+            and self.chances_left == other.chances_left
+        )
+
+    def __hash__(self) -> int:
+        return hash(
+            (
+                self.correct_guesses,
+                self.wrong_index_guesses,
+                self.wrong_guesses,
+                self.chances_left,
+            )
+        )
+
+    def __str__(self) -> str:
+        return (
+            f"CG: {''.join(self.correct_guesses)}; WI: {''.join(self.wrong_index_guesses)}; "
+            f"WG: {''.join(self.wrong_guesses)}; Chances Left: {self.chances_left}"
+        )
 
 
 class WordleAI:
@@ -129,7 +170,7 @@ class WordleAI:
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        return self.q.get((tuple(state), action), 0)
+        return self.q.get((state, action), 0)
 
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
@@ -146,7 +187,7 @@ class WordleAI:
         `alpha` is the learning rate, and `new value estimate`
         is the sum of the current reward and estimated future rewards.
         """
-        self.q[(tuple(state), action)] = old_q + self.alpha * (
+        self.q[(state, action)] = old_q + self.alpha * (
             (future_rewards + reward) - self.get_q_value(state, action)
         )
 
@@ -161,7 +202,6 @@ class WordleAI:
         `state`, return 0.
         """
         best_reward = 0
-        state = tuple(state)
         for (curr_state, _), q_value in self.q.items():
             if curr_state == state:
                 if q_value > best_reward:
@@ -186,21 +226,19 @@ class WordleAI:
 
         global ALL_WORDS
 
-        state_actions = []
-        state = tuple(state)
+        best_action = ""
+        best_value = -float("inf")
         for curr_state, curr_action in self.q.keys():
             if curr_state == state:
-                state_actions.append(curr_action)
+                if best_value < self.q[(curr_state, curr_action)]:
+                    best_value = self.q[(curr_state, curr_action)]
+                    best_action = curr_action
 
         random_action = random.choice(ALL_WORDS)
 
-        if not state_actions:
+        if best_action == "":
             return random_action
 
-        best_action = max(
-            state_actions,
-            key=lambda curr_action: self.q[(state, curr_action)],
-        )
         if epsilon:
             chosen_action = random.choices(
                 [best_action, random_action], weights=[1 - self.epsilon, self.epsilon]
@@ -211,28 +249,25 @@ class WordleAI:
         return chosen_action
 
     def get_state(self, wordle):
-        return (
-            tuple(wordle.guessed_correct),
-            tuple(wordle.guessed_wrong_index),
-            tuple(wordle.guessed_correct),
-            wordle.chance_remaining,
+        data = State(
+            correct_guesses=wordle.guessed_correct,
+            wrong_index_guesses=wordle.guessed_wrong_index,
+            wrong_guesses=wordle.guessed_wrong,
+            chances_left=wordle.chance_remaining,
         )
+        return data
 
-    def get_reward(self, old_state, new_state):
-        old_correct_guesses, old_wrong_index_guesses, old_wrong_guesses, _ = old_state
-        (
-            new_correct_guesses,
-            new_wrong_index_guesses,
-            new_wrong_guesses,
-            new_chance_remaining,
-        ) = new_state
-
+    def get_reward(self, old_state, new_state, win=False, lose=False):
         reward = 0
-        reward += (len(new_correct_guesses) - len(old_correct_guesses)) * 10
-        reward += (len(new_wrong_index_guesses) - len(old_wrong_index_guesses)) * 5
-        reward -= (len(new_wrong_guesses) - len(old_wrong_guesses)) * (
-            5 + (6 - new_chance_remaining)
-        )
+        reward += (len(new_state.correct_guesses) - len(old_state.correct_guesses)) * 10
+        reward += (
+            len(new_state.wrong_index_guesses) - len(old_state.wrong_index_guesses)
+        ) * 5
+        reward -= (len(new_state.wrong_guesses) - len(old_state.wrong_guesses)) * 5
+        if win:
+            reward += 100
+        if lose:
+            reward -= 100
         return reward
 
 
@@ -248,32 +283,27 @@ def train_ai(n):
 
             wordle.play(guess)
             new_state = ai.get_state(wordle)
+            reward = ai.get_reward(
+                old_state,
+                new_state,
+                win=wordle.win,
+                lose=wordle.terminal and not wordle.win,
+            )
 
-            # if not wordle.win:
-            #     ai.update(
-            #         old_state,
-            #         guess,
-            #         new_state,
-            #         reward=ai.get_reward(old_state, new_state),
-            #     )
-
-            # if wordle.terminal:
-            #     ai.update(old_state, guess, new_state, reward=100)
+            ai.update(old_state, guess, new_state, reward)
 
     return ai
 
-    # ai = train_ai(100)
-            # old_state = ai.get_state(wordle)
-            # guess = ai.choose_action(old_state)
 
 def main():
-    while True:
-        wordle = Wordle()
-        while not wordle.terminal:
+    ai = train_ai(99999999)
+    wordle = Wordle()
+    while not wordle.terminal:
+        old_state = ai.get_state(wordle)
+        guess = ai.choose_action(old_state)
 
-            guess = random.choice(ALL_WORDS)
-            print("Guess:", guess)
-            wordle.play(guess)
+        print("Guess:", guess)
+        wordle.play(guess)
 
 
 main()
